3082265ef019c9357ddf762ab449a14c369d447b
Kirsten <kisten.martenson@gmail.com>
2019-03-20 11:16:30 -0500

    finished MVP features

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -42,7 +42,8 @@ export default {
         .then(response => {
           axios.defaults.headers.common["Authorization"] = "Bearer " + response.data.jwt;
           localStorage.setItem("jwt", response.data.jwt);
-          this.$router.push("/");
+          localStorage.setItem("user_id", response.data.user_id);
+          this.$router.push("/users/me");
         })
         .catch(error => {
           this.errors = ["Invalid email or password."];
--- src/views/users/Show.vue
+++ src/views/users/Show.vue
@@ -5,14 +5,13 @@
       <h4>Email: {{ user.email }}'s Profile</h4>
       <br>
       <H3>My favorites</H3>
-      <div v-for="pairing in pairings">
-        <div class="card-body">
-          <h4 class="card-title">Pairing: {{pairing.id }}</h4>
-          <p class="card-text">Wine: {{wine.id }}</p>
-          <p class="card-text">Food: {{food.id }}</p>
-        </div>
+      <div v-for="favorite in user.favorites">
+        <router-link v-bind:to="'/pairings/' + favorite.pairing_id">
+          <h5>favorite.pairing_id</h5>
+        </router-link>
       </div>
     </div>
+    <router-link v-bind:to="'/users/me/edit'">Edit my profile</router-link>
   </div>
 </template>
 
